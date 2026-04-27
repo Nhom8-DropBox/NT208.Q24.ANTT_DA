e5fc2a2d56cb0a09a10f6454875d0998d5aad2b5
van-767 <van767.1973@gmail.com>
2026-04-27 01:38:16 +0000

    logout+refresh

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,46 +14,14 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 
-// Middleware
 app.use(express.json());
 
-// route
+app.use(middlewareAuth); 
+
 app.use('/auth', authRoute);
 
-// Test route
-
-app.get("/", (req, res) => {
-  res.send("API is running 🚀");
-});
-
-app.use(middlewareAuth); // Apply authentication middleware to all routes after login (have jwt token)
-
-// route cho dashboard
 app.use(`/dashboard`, dashboardRoute);
 
-// app.us(auth.js)
-
-
-// Test DB connection
-// app.get("/db-test", async (req, res) => {
-//   try {
-//     req.user = { id: 1 }; // Giả sử user đã được xác thực và có ID là 1 
-//     const result = await pool.query("SELECT NOW()");
-//     res.json({
-//       success: true,
-//       time: result.rows[0],
-//     });
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({
-//       success: false,
-//       error: "Database connection failed",
-//     });
-//   }
-// });
-
-
-// Start server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
