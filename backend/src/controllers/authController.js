1223c4476e12d12d85ea7f369c8071165626ebaf
van-767 <van767.1973@gmail.com>
2026-04-27 02:15:40 +0000

    Get profile

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -43,6 +43,10 @@ const authController = {
                 return res.status(400).json({message: 'Tài khoản không hợp lệ'});
             }
             const passwordMatch = await bcrypt.compare(password, user.password_hash);
+            if (!passwordMatch)
+            {
+                return res.status(400).json({message: 'Mật khẩu không đúng'});
+            }
             const accessToken = jwt.sign(
                 {userID: user.id},
                 process.env.JWT_SECRET,
@@ -111,6 +115,29 @@ const authController = {
 
         res.json({message: 'Đăng xuất thành công'});
     },
+
+    getProfile: async (req, res) =>
+    {
+        const userID = req.user.userID;
+        try
+        {
+            const result = await pool.query(
+                "select email from users where id = $1",
+                [userID]
+            );
+
+            const sumStorage = await pool.query(
+                "select sum(fv.size_bytes) as total_storage from files f join users u on f.owner_id = u.id join file_versions fv on f.id = fv.file_id where u.id = $1 and f.deleted_at is NULL",
+                [userID]
+            );
+            res.json({ user: result.rows[0], total_storage: sumStorage.rows[0].total_storage });
+        }
+        catch (err)
+        {
+            console.log('Error', err);
+            res.status(500).json({message: 'Lỗi Server'});
+        }
+    }
 }
 
 export default authController;
--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,4 +1,5 @@
 import authController from '../controllers/authController.js';
+import middlewareAuth from '../middleware/auth.js';
 import express from 'express';
 const router = express.Router();
 
@@ -7,4 +8,5 @@ router.post('/register', authController.register);
 router.post('/login', authController.login); 
 router.post('/refresh', authController.refresh);
 router.post('/logout', authController.logout);
+router.get('/profile', middlewareAuth, authController.getProfile);
 export default router;
