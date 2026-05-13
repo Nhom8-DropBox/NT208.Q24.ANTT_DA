c307c61cc1cf7ba14c211eca3cd061800809072e
van-767 <van767.1973@gmail.com>
2026-05-13 17:32:17 +0000

    fix Content-Type case

--- frontend/src/pages/register.jsx
+++ frontend/src/pages/register.jsx
@@ -30,7 +30,7 @@ function Register(){
                     method: "POST",
 
                     headers: {
-                        "Content-Type": "application/JSON"
+                        "Content-Type": "application/json"
                     },
                     body: JSON.stringify(data)
                 }
