20fab0e24fabbc429357ff971459d449c3c51a27
ngovantinh-ops-pwn <ngovantinh27092005@gmail.com>
2026-05-14 14:16:00 +0700

    hi

--- backend/src/routes/files.js
+++ backend/src/routes/files.js
@@ -4,6 +4,7 @@ import listController from "../controllers/listController.js";
 import recycleController from "../controllers/recycleController.js";
 
 
+
 const router = express.Router();
 
 // upload routes
@@ -20,7 +21,7 @@ router.get("/trash", listController.getTrash);
 router.post("/resolve", listController.resolveFile);
 
 // Dynamic routes
-router.get("/:id", listController.getFileById); // preview, ch có fe
+router.get("/:id", listController.getFileById);
 router.delete("/:id", listController.deleteFile);
 router.get("/:id/download-url", fileController.GetPresignedDownloadURL);
 router.get("/:id/versions", listController.getFileVersions);
@@ -28,6 +29,8 @@ router.get("/:id/versions/:versionNo/download-url", listController.getVersionDow
 router.post("/:id/versions/:versionNo/restore", listController.restoreVersion);
 
 //trashbin
-router.post("/:id/restore" , recycleController.restore )
+router.post("/:id/restore" , recycleController.restore );
+router.delete("/:id/permanent", recycleController.deleteForever);
+
 
 export default router;
